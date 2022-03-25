48452bbe7d3820df52b972d5e9c69e95ad7e7866
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-03-25 16:20:56 -0700

    :sparkles: Add first working Helm templates

--- Tiltfile
+++ Tiltfile
@@ -3,9 +3,6 @@
 # For more information, visit https://docs.tilt.dev.
 
 local_resource("setup", "./setup.sh")
-docker_build("backend", "./backend")
+docker_build("acronyms-backend", "./backend")
 
 k8s_yaml("acronyms.yaml")
-
-k8s_resource("backend", port_forwards=8000)
-k8s_resource("postgres", port_forwards=5432)
--- backend/src/acronyms/__main__.py
+++ backend/src/acronyms/__main__.py
@@ -1,59 +1,10 @@
-"""Command line interface for acronyms.
-
-See https://docs.python.org/3/using/cmdline.html#cmdoption-m for why module is
-named __main__.py.
-"""
+import sys
+import uvicorn
 
 
-import logging
-from typing import Any, Dict, Optional
-
-from fastapi import Depends, FastAPI
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
-
-from acronyms import models
-from acronyms.models import Acronym
+def main():
+    uvicorn.main(["acronyms.site:app", *sys.argv[1:]])
 
 
-app = FastAPI()
-
-
-class AcronymBody(BaseModel):
-    """Post request validator for Acronym type."""
-
-    abbreviation: str
-    expansion: str
-
-
-@app.get("/")
-async def root() -> Dict[str, Any]:
-    """Greet user."""
-    logging.debug("root path accessed")
-    return {"message": "Hello World"}
-
-
-@app.get("/acronyms")
-async def get_acronym(
-    abbreviation: Optional[str] = None,
-    session: Session = Depends(models.get_db),
-) -> Optional[Any]:
-    """Get all matching acronyms."""
-    query = session.query(Acronym.abbreviation, Acronym.expansion)
-
-    if abbreviation is not None:
-        query = query.filter(Acronym.abbreviation == abbreviation)
-
-    return query.all()
-
-
-@app.post("/acronyms")
-async def post_acronym(
-    acronym: AcronymBody, session: Session = Depends(models.get_db)
-) -> None:
-    """Insert an acronym to database."""
-    acronym_ = Acronym(
-        abbreviation=acronym.abbreviation, expansion=acronym.expansion
-    )
-    session.add(acronym_)
-    session.commit()
+if __name__ == "__main__":
+    main()
