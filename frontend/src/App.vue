3bb413567b484da1f474172de90fa73a02f12640
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-03-31 18:52:21 -0700

    :sparkles: Add Bulma CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,51 @@
 <template>
-  <h1>Acronyms</h1>
-  <table>
-    <tr>
-      <th>Abbreviation</th>
-      <th>Expansion</th>
-    </tr>
-    <tr v-for="acronym in acronyms">
-      <td>{{ acronym.abbreviation }}</td>
-      <td>{{ acronym.expansion }}</td>
-    </tr>
+  <div class="container">
+    <div class="field">
+      <label class="label">Search</label>
+      <div class="control has-icons-left has-icons-right">
+        <input
+          class="input is-success"
+          type="text"
+          placeholder="Search"
+          v-model="search"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-user"></i>
+        </span>
+        <span class="icon is-small is-right">
+          <i class="fas fa-check"></i>
+        </span>
+      </div>
+    </div>
+  </div>
+  <table class="container table mt-4">
+    <thead>
+      <tr>
+        <th>Abbreviation</th>
+        <th>Expansion</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="acronym in acronyms">
+        <td>{{ acronym.abbreviation }}</td>
+        <td>{{ acronym.expansion }}</td>
+      </tr>
+    </tbody>
   </table>
 </template>
 
 <script lang="ts">
 const acronyms = [
   { id: 1, abbreviation: "ROI", expansion: "Return On Investment" },
-  { id: 2, abbreviation: "DM", expansion: "Data Mining"},
-  { id: 3, abbreviation: "DM", expansion: "Direct Message"},
+  { id: 2, abbreviation: "DM", expansion: "Data Mining" },
+  { id: 3, abbreviation: "DM", expansion: "Direct Message" },
 ];
 
 export default {
   data() {
-    return { acronyms, count: 0 };
-  }
-}
+    return { acronyms, search: "" };
+  },
+};
 </script>
 
 <style>
