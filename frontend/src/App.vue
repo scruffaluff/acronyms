f994c34eb0f5b298aeeba2ad5b4b562a5654dead
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-04-05 23:18:53 -0700

    :lipstick: Add navbar burger

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,65 +1,89 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-menu" style="margin-right: 0px">
-      <div class="navbar-start">
-        <a class="navbar-item title is-4 has-text-primary">Acronyms</a>
-      </div>
+  <nav class="navbar p-4 px-6" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <div class="navbar-item title mb-0 is-4 has-text-primary">Acronyms</div>
+      <a
+        aria-label="menu"
+        aria-expanded="false"
+        class="navbar-burger"
+        data-target="login"
+        role="button"
+        :class="{ 'is-active': navBarBurger }"
+        @click="navBarBurger = !navBarBurger"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu" id="login" :class="{ 'is-active': navBarBurger }">
       <div class="navbar-end">
         <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light">Log in</a>
-          </div>
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+        </div>
+        <div class="navbar-item">
+          <a class="button is-light">Log in</a>
         </div>
       </div>
     </div>
   </nav>
 
-  <div class="mx-6 my-6 container">
-    <div class="field">
-      <div class="control has-icons-left has-icons-right">
-        <input
-          class="input"
-          placeholder="Search"
-          type="text"
-          v-model="search"
-        />
-        <span class="icon is-left is-small">
-          <i class="fas fa-search"></i>
-        </span>
+  <main class="main section">
+    <div class="mx-6 my-6 container">
+      <div class="field">
+        <div class="control has-icons-left has-icons-right">
+          <input
+            class="input"
+            placeholder="Search"
+            type="text"
+            v-model="search"
+          />
+          <span class="icon is-left is-small">
+            <i class="fas fa-search"></i>
+          </span>
+        </div>
       </div>
     </div>
-  </div>
 
-  <table class="container table is-fullwidth is-hoverable has-text-left">
-    <thead>
-      <tr>
-        <th v-for="column in columns">
-          {{ column.name }}
-          <span class="icon is-small" @click="switchSort(column.name)">
-            <i class="fas" :class="column.icon"></i>
-          </span>
-        </th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="acronym in acronymsFiltered">
-        <td>{{ acronym.abbreviation }}</td>
-        <td>{{ acronym.expansion }}</td>
-        <td>
-          <span class="icon mx-1 is-right" @click="put(acronym.id)">
-            <i class="fas fa-pencil"></i>
-          </span>
-          <span class="icon mx-1 is-right" @click="remove(acronym.id)">
-            <i class="fas fa-trash-can"></i>
-          </span>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+    <table class="container table is-fullwidth is-hoverable has-text-left">
+      <thead>
+        <tr>
+          <th v-for="column in columns">
+            {{ column.name }}
+            <span class="icon is-small" @click="switchSort(column.name)">
+              <i class="fas" :class="column.icon"></i>
+            </span>
+          </th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="acronym in acronymsFiltered">
+          <td>{{ acronym.abbreviation }}</td>
+          <td>{{ acronym.expansion }}</td>
+          <td>
+            <span class="icon mx-1 is-right" @click="put(acronym.id)">
+              <i class="fas fa-pencil"></i>
+            </span>
+            <span class="icon mx-1 is-right" @click="remove(acronym.id)">
+              <i class="fas fa-trash-can"></i>
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
+
+  <footer class="footer py-6">
+    <div class="container">
+      <h4>
+        <strong>Acronyms</strong> by
+        <a href="https://github.com/scruffaluff">Scruffaluff</a>.
+      </h4>
+    </div>
+  </footer>
 </template>
 
 <script setup lang="ts">
@@ -119,6 +143,7 @@ let columns = reactive([
   { name: "abbreviation", ascending: true, icon: "fa-arrow-up" },
   { name: "expansion", ascending: true, icon: "fa-arrow-up" },
 ]);
+const navBarBurger = ref(false);
 const search = ref("");
 
 const acronymsFiltered = computed(() => {
@@ -141,6 +166,12 @@ onMounted(fetchData);
 </script>
 
 <style>
+.main {
+  flex: 1;
+}
+.title {
+  margin-bottom: 0;
+}
 th {
   text-transform: capitalize;
 }
