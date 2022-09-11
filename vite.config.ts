87b69ae273c8f2ac10c60ac386a6a5a994db0c5f
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-09-10 17:28:15 -0700

    :lipstick: Improve favicons

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicDir: "frontend/public",
   test: {
     include: ["tests/frontend/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
