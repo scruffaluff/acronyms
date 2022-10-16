859cf9d6fac7d1e63c6411bc77b4822122ea2fdb
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-10-16 08:27:48 -0700

    :lipstick: Improve favicon color

--- scripts/serve_devsite.sh
+++ scripts/serve_devsite.sh
@@ -17,22 +17,11 @@ main() {
   # Switch current directory to repository root.
   cd "$(dirname "$(dirname "$(realpath "$0")")")"
 
-  mkdir -p certs
-  if [[ ! (-f certs/wildcard_nip_io.crt && -f certs/wildcard_nip_io.key) ]]; then
-    mkcert \
-      -cert-file certs/wildcard_nip_io.crt \
-      -key-file certs/wildcard_nip_io.key \
-      127-0-0-1.nip.io
-  fi
-
   # Build once so that files are guaranteed to exist before backend starts.
   npm run build -- --mode development
   poetry run acronyms \
-    --host 127-0-0-1.nip.io \
     --reload \
-    --reload-dir backend/acronyms \
-    --ssl-certfile certs/wildcard_nip_io.crt \
-    --ssl-keyfile certs/wildcard_nip_io.key &
+    --reload-dir backend/acronyms &
 
   # Sleeping for 1 second prevents vite from making folder "dist/assets"
   # temporarily unavailable to the backend server.
